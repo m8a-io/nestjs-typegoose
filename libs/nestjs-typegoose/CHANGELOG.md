56c3df7fee89d2eca14ba92a675acdc8f1cc6fe7
Scott Molinari <scott.molinari@adduco.de>
2023-08-28 08:56:06 +0000

    chore(all): merge dev into main

---
+++ libs/nestjs-typegoose/CHANGELOG.md
@@ -0,0 +1,32 @@
+# Change Log - @m8a/nestjs-typegoose
+
+This log was last generated on Tue, 11 Jul 2023 08:45:06 GMT and should not be manually modified.
+
+## 11.0.2
+Tue, 11 Jul 2023 08:45:06 GMT
+
+### Patches
+
+- fix allowed class type
+
+## 11.0.1
+Thu, 06 Jul 2023 13:51:59 GMT
+
+### Patches
+
+- Update peer dependencies for Nestjs
+
+## 11.0.0
+Wed, 29 Mar 2023 08:03:00 GMT
+
+### Breaking changes
+
+- update to newest typegoose v11 and mongoose v7
+
+## 10.1.3
+Wed, 15 Mar 2023 13:28:34 GMT
+
+### Patches
+
+- Initial change log entry
+
---
+++ libs/nestjs-typegoose/website/.docusaurus/registry.js
@@ -0,0 +1,87 @@
+export default {
+  '0480b142': [
+    () => import(/* webpackChunkName: '0480b142' */ '@site/docs/faq.md'),
+    '@site/docs/faq.md',
+    require.resolveWeak('@site/docs/faq.md')
+  ],
+  '16c91d09': [
+    () => import(/* webpackChunkName: '16c91d09' */ '@site/docs/async-configuration.md'),
+    '@site/docs/async-configuration.md',
+    require.resolveWeak('@site/docs/async-configuration.md')
+  ],
+  17896441: [
+    () => import(/* webpackChunkName: '17896441' */ '@theme/DocItem'),
+    '@theme/DocItem',
+    require.resolveWeak('@theme/DocItem')
+  ],
+  '1be78505': [
+    () => import(/* webpackChunkName: '1be78505' */ '@theme/DocPage'),
+    '@theme/DocPage',
+    require.resolveWeak('@theme/DocPage')
+  ],
+  '240e7a45': [
+    () =>
+      import(
+        /* webpackChunkName: '240e7a45' */ '/home/dev/m8a/deps/nestjs-typegoose/website/.docusaurus/docusaurus-plugin-content-pages/default/plugin-route-context-module-100.json'
+      ),
+    '/home/dev/m8a/deps/nestjs-typegoose/website/.docusaurus/docusaurus-plugin-content-pages/default/plugin-route-context-module-100.json',
+    require.resolveWeak(
+      '/home/dev/m8a/deps/nestjs-typegoose/website/.docusaurus/docusaurus-plugin-content-pages/default/plugin-route-context-module-100.json'
+    )
+  ],
+  '4020c8f9': [
+    () => import(/* webpackChunkName: '4020c8f9' */ '@site/docs/schema-options.md'),
+    '@site/docs/schema-options.md',
+    require.resolveWeak('@site/docs/schema-options.md')
+  ],
+  '5e9f5e1a': [
+    () => import(/* webpackChunkName: '5e9f5e1a' */ '@generated/docusaurus.config'),
+    '@generated/docusaurus.config',
+    require.resolveWeak('@generated/docusaurus.config')
+  ],
+  '6476eba6': [
+    () => import(/* webpackChunkName: '6476eba6' */ '@site/docs/usage.md'),
+    '@site/docs/usage.md',
+    require.resolveWeak('@site/docs/usage.md')
+  ],
+  '935f2afb': [
+    () => import(/* webpackChunkName: '935f2afb' */ '~docs/default/version-current-metadata-prop-751.json'),
+    '~docs/default/version-current-metadata-prop-751.json',
+    require.resolveWeak('~docs/default/version-current-metadata-prop-751.json')
+  ],
+  '99f6fac0': [
+    () => import(/* webpackChunkName: '99f6fac0' */ '@site/docs/discriminators.md'),
+    '@site/docs/discriminators.md',
+    require.resolveWeak('@site/docs/discriminators.md')
+  ],
+  c4de80f8: [
+    () => import(/* webpackChunkName: 'c4de80f8' */ '@site/docs/install.md'),
+    '@site/docs/install.md',
+    require.resolveWeak('@site/docs/install.md')
+  ],
+  c4f5d8e4: [
+    () => import(/* webpackChunkName: 'c4f5d8e4' */ '@site/src/pages/index.js'),
+    '@site/src/pages/index.js',
+    require.resolveWeak('@site/src/pages/index.js')
+  ],
+  eb54544b: [
+    () =>
+      import(
+        /* webpackChunkName: 'eb54544b' */ '/home/dev/m8a/deps/nestjs-typegoose/website/.docusaurus/docusaurus-plugin-content-docs/default/plugin-route-context-module-100.json'
+      ),
+    '/home/dev/m8a/deps/nestjs-typegoose/website/.docusaurus/docusaurus-plugin-content-docs/default/plugin-route-context-module-100.json',
+    require.resolveWeak(
+      '/home/dev/m8a/deps/nestjs-typegoose/website/.docusaurus/docusaurus-plugin-content-docs/default/plugin-route-context-module-100.json'
+    )
+  ],
+  fb7b7bcb: [
+    () => import(/* webpackChunkName: 'fb7b7bcb' */ '@site/docs/multiple-connections.md'),
+    '@site/docs/multiple-connections.md',
+    require.resolveWeak('@site/docs/multiple-connections.md')
+  ],
+  fbe93038: [
+    () => import(/* webpackChunkName: 'fbe93038' */ '@site/docs/testing.md'),
+    '@site/docs/testing.md',
+    require.resolveWeak('@site/docs/testing.md')
+  ]
+}
